b0bfea86257bc0d93f9d5e73fa2eda2cac42282c
zdenizcelik <deniz.celik@arcelik.com>
2016-06-13 14:31:46 +0300

    Recommendation reason is added to recommendations.

--- imdb/serverapp/views/movie.py
+++ imdb/serverapp/views/movie.py
@@ -37,18 +37,15 @@ def show(request, movie_id):
 	storyline = open(storyline_path).read()
 	synopsis_path = os.path.join(settings.SYNOPSIS, "{0}".format(movie.id))
 	synopsis = open(synopsis_path).read()
-	movie_ids, important_fields = RecommendationUnit.get_similar_movies(movie, 1, 1, 1, 1, 1, 1)
-	doc_index = {id: movie_ids.index(id) for id in movie_ids[:9]}
-	print "movie_ids: ", movie_ids
-	print "important_fields: ", important_fields
-	movies = models.Movie.objects.filter(id__in=doc_index.keys())[:9]
-	recommended_movies = [0] * len(movies)
-	i = 0
-	for movie in movies:
-		new_index = doc_index[movie.id]
-		# recommended_movies[new_index] = (movie, important_fields[i])
-		recommended_movies[new_index] = movie
-		i += 1
+	recommendations = RecommendationUnit.get_similar_movies(movie, 1, 1, 1, 1, 1, 1)
+	print "recommendations: ", recommendations
+	recommended_movies = []
+	for recommendation in recommendations:
+		movie_title = models.Movie.objects.get(id=recommendation[0]).title
+		new_tuple = (movie_title,) + recommendation
+		recommended_movies.append(new_tuple)
+	print "recommended_movies :", recommended_movies
+
 	content = {
 		"movie": movie,
 		"storyline": storyline,
