7ba56a4d691d54ca3689cbcb34116fba6f5af768
zdenizcelik <deniz.celik@arcelik.com>
2016-09-09 16:22:12 +0300

    Final changes.

--- imdb/crawler.py
+++ imdb/crawler.py
@@ -21,10 +21,6 @@ class ImdbSpider(scrapy.Spider):
 	start_urls = ['http://www.imdb.com/chart/top?ref_=ft_250']
 	
 	def parse(self, response):
-		print "yeeeeey"
-		print "yeeeeey"
-		print "yeeeeey"
-		print "yeeeeey"
 		for href in response.css('.titleColumn a::attr(href)'):
 			full_url = response.urljoin(href.extract())
 			yield scrapy.Request(full_url, callback=self.parse_movie)
@@ -46,40 +42,38 @@ class ImdbSpider(scrapy.Spider):
 					director=director
 				)
 
-				if movie_created:
-					crawled_genres = response.css('span[itemprop=genre]::text').extract()
-					for crawled_genre in crawled_genres:
-						genre, created = models.Genre.objects.get_or_create(name=crawled_genre)
-						models.MovieGenre.objects.get_or_create(movie=movie, genre=genre)
+				crawled_genres = response.css('span[itemprop=genre]::text').extract()
+				for crawled_genre in crawled_genres:
+					genre, created = models.Genre.objects.get_or_create(name=crawled_genre)
+					models.MovieGenre.objects.get_or_create(movie=movie, genre=genre)
 
-					crawled_writers = response.css('.credit_summary_item span[itemprop=creator] a span::text').extract()
-					for crawled_writer in crawled_writers:
-						writer, created = models.Writer.objects.get_or_create(name=crawled_writer)
-						models.MovieWriter.objects.get_or_create(movie=movie, writer=writer)
+				crawled_writers = response.css('.credit_summary_item span[itemprop=creator] a span::text').extract()
+				for crawled_writer in crawled_writers:
+					writer, created = models.Writer.objects.get_or_create(name=crawled_writer)
+					models.MovieWriter.objects.get_or_create(movie=movie, writer=writer)
 
-					crawled_actors = response.css('.cast_list td[itemprop=actor] a span::text').extract()
-					for crawled_actor in crawled_actors:
-						actor, created = models.Actor.objects.get_or_create(name=crawled_actor)
-						models.MovieActor.objects.get_or_create(movie=movie, actor=actor)
+				crawled_actors = response.css('.cast_list td[itemprop=actor] a span::text').extract()
+				for crawled_actor in crawled_actors:
+					actor, created = models.Actor.objects.get_or_create(name=crawled_actor)
+					models.MovieActor.objects.get_or_create(movie=movie, actor=actor)
 
 		except Exception as e:
 			print "Error occured: {0}".format(e)
 			return
 
-		if movie_created:
-			crawled_poster = sel.xpath("//div[@class='poster']/a/img/@src").extract()[0]
-			poster_file = os.path.join(settings.POSTERS, u"{0}.jpg".format(movie.id))
-			urllib.urlretrieve(crawled_poster, poster_file)
+		crawled_poster = sel.xpath("//div[@class='poster']/a/img/@src").extract()[0]
+		poster_file = os.path.join(settings.POSTERS, u"{0}.jpg".format(movie.id))
+		urllib.urlretrieve(crawled_poster, poster_file)
 
-			crawled_storyline = response.css('#titleStoryLine .inline[itemprop=description] p::text').extract()[0].strip()
-			storyline_file_path = os.path.join(settings.STORYLINE, "{0}".format(movie.id))
-			storyline_file = codecs.open(storyline_file_path, "a", "utf-8")
-			storyline_file.write(crawled_storyline)
-			storyline_file.close()
+		crawled_storyline = response.css('#titleStoryLine .inline[itemprop=description] p::text').extract()[0].strip()
+		storyline_file_path = os.path.join(settings.STORYLINE, "{0}".format(movie.id))
+		storyline_file = codecs.open(storyline_file_path, "a", "utf-8")
+		storyline_file.write(crawled_storyline)
+		storyline_file.close()
 
-			crawled_synopsis_link = sel.xpath("//div[@id='titleStoryLine']//a[contains(text(), 'Plot Synopsis')]/@href").extract()[0]
-			full_url = response.urljoin(crawled_synopsis_link)
-			yield scrapy.Request(full_url, callback=self.parse_synopsis, meta={'movie_id': movie.id, 'success': "{0} crawled.".format(movie)})
+		crawled_synopsis_link = sel.xpath("//div[@id='titleStoryLine']//a[contains(text(), 'Plot Synopsis')]/@href").extract()[0]
+		full_url = response.urljoin(crawled_synopsis_link)
+		yield scrapy.Request(full_url, callback=self.parse_synopsis, meta={'movie_id': movie.id, 'success': "{0} crawled.".format(movie)})
 
 	@callback_args
 	def parse_synopsis(self, response, movie_id, success):
